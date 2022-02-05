e275b144afd9b03206fba6996eab510e4eee7280
Jordan Strobing <jordanstrobing@gmail.com>
2022-02-04 20:52:45 -0500

    Third commit - added a few things that may help

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # AnalyzeData - Script to analyze the .txt files from GreatSPN
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, simpledialog
+import re
 
 # These lines open a prompt asking the user what file to open
 root = tk.Tk()
@@ -10,7 +11,7 @@ file_path = filedialog.askopenfilename() # This saves the path of the file we ne
 # We need to make it so it only accepts .txt, should be pretty simple but not important yet
 
 
-# This reads all the files
+# This reads every line of the file
 with open(file_path) as f:
     lines = f.readlines()
 
@@ -32,7 +33,7 @@ f = open(newFile, 'w')
 # This saves all the times and exports them to the file
 # Might be useless but it could help for reference, maybe.
 # If it is useful, we can definitely clean it up
-times = []
+'''times = []
 i = 0
 for line in lines:
     for x in range(len(line)):
@@ -40,7 +41,18 @@ for line in lines:
             times.append(line[x:x+6])
             i += 1
 for t in times:
-    f.write(t + "\n")
+    f.write(t + "\n")'''
+
+# This is a pop up box to ask the user the name of the place, saves it to placeName
+placeName = simpledialog.askstring(title="Place Information", prompt="Enter the name of the place you'd like to get data for: (case sensitive)")
+
+# This loop checks if the place name entered is in each line. Can use this to help get the number.
+# We can use this logic as a foundation to figuring out how to extract the numbers
+for line in lines:
+    if placeName in line:
+        print("true")
+    else:
+        print("false")
 
 # We need to close the file in order for the program to stop
 f.close()
