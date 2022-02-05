f53dd166df24bc0047eac7be5f2d8366966b5375
Jordan Strobing <jordanstrobing@gmail.com>
2022-02-05 16:15:31 -0500

    Fourth commit - was able to use Regex to find data we need

--- main.py
+++ main.py
@@ -45,14 +45,28 @@ for t in times:
 
 # This is a pop up box to ask the user the name of the place, saves it to placeName
 placeName = simpledialog.askstring(title="Place Information", prompt="Enter the name of the place you'd like to get data for: (case sensitive)")
+placeName = placeName + '='
 
 # This loop checks if the place name entered is in each line. Can use this to help get the number.
 # We can use this logic as a foundation to figuring out how to extract the numbers
-for line in lines:
+'''for line in lines:
     if placeName in line:
         print("true")
     else:
-        print("false")
+        print("false")'''
+
+# This makes it so that the only thing that prints is the place chosen and how many tokens it has
+# Tracks place as long as it has 1 or more tokens
+# For later, need to get the times to show up and concatenate them
+for line in lines:
+    if placeName in line:
+        n = re.search(r'%s.*[^,]*' % placeName, line)
+        t = re.search(r'@.*[^\s]*', line)
+        n.group()
+        t.group()
+        head, sep, tail = n.group().partition(',')
+        timehead, timesep, timetail = t.group().partition(' ')
+        f.write(timehead + " " + head + "\n")
 
 # We need to close the file in order for the program to stop
 f.close()
